b665206821489530f1690cd45c790f753fc95d26
riyaiWebSoul <145985137+riyaiWebSoul@users.noreply.github.com>
2023-10-10 17:10:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,12 +26,12 @@ const PORT = process.env.PORT || 8080;
 // Connect to the MongoDB database
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/your-database-name', {
+    const db = await mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/your-database-name', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('Database connected');
-    return mongoose.connection; // Return the connection object
+    return db; // Return the MongoDB connection object
   } catch (err) {
     console.error('Error connecting to the database:', err);
     throw err;
@@ -45,9 +45,9 @@ server.use(morgan('default'));
 // Serve static files from the 'public' directory
 server.use(express.static('public'));
 server.use(cors({
-  origin:["https://deploy-mean-1whq.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
+  origin: ["https://deploy-mean-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
 }));
 
 // Configure Multer for handling file uploads
@@ -82,26 +82,7 @@ async function setupRoutes() {
   server.use('/imageUpload', ImageUploadRouter.router);
   server.use('/loginId', LoginIdRouter.router);
   server.use('/images', express.static('public/images'));
-
-  // Add a route to fetch data from MongoDB
-  server.get('/getDataFromMongoDB', async (req, res) => {
-    try {
-      const db = await connectToDatabase(); // Connect to the database
-      const collection = db.collection('eikon'); // Replace with your collection name
-
-      // Query MongoDB to retrieve data (modify this query as needed)
-      const data = await collection.find({}).toArray();
-
-      // Send the retrieved data as a JSON response
-      res.json({ data });
-    } catch (err) {
-      console.error('Error fetching data from MongoDB:', err);
-      res.status(500).json({ error: 'Error fetching data from MongoDB' });
-    }
-  });
 }
-
-const imageUrls = [];
 
 server.get('/listImages', (req, res) => {
   const imageDir = path.join(__dirname, 'public', 'images');
@@ -125,44 +106,21 @@ server.get('/listImages', (req, res) => {
   });
 });
 
-server.post('/imageUpload', upload.single('image'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
+// Add a route to fetch data from MongoDB
+server.get('/getDataFromMongoDB', async (req, res) => {
+  try {
+    const db = await connectToDatabase(); // Connect to the database
+    const collection = db.collection('eikon'); // Replace with your collection name
+
+    // Query MongoDB to retrieve data (modify this query as needed)
+    const data = await collection.find({}).toArray();
+
+    // Send the retrieved data as a JSON response
+    res.json({ data });
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).json({ error: 'Error fetching data from MongoDB' });
   }
-
-  // Get the image name from the uploaded file's filename
-  const imageName = req.file.filename;
-  console.log(imageName);
-
-  // Add the image name to the imageUrls array
-  imageUrls.push(imageName);
-
-  // You can send back the updated imageUrls array as a response
-  res.json({ imageUrl: `/${imageName}` });
-
-  // You can do further processing with the uploaded file here
-  // For now, just send a success response
-  res.send('File uploaded successfully.');
-});
-
-server.delete('/deleteImage/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const imagePath = path.join(__dirname, 'public', 'images', filename);
-
-  // Use the 'fs' module to delete the image file
-  fs.unlink(imagePath, (err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error deleting image' });
-    }
-
-    // Remove the deleted image URL from your 'imageUrls' array if needed
-    const index = imageUrls.indexOf(`/images/${filename}`);
-    if (index !== -1) {
-      imageUrls.splice(index, 1);
-    }
-
-    res.json({ message: 'Image deleted successfully' });
-  });
 });
 
 // Start the server on port 8080
