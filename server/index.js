5c9d704c7c04c971663c70db2abdee063a8f33bb
riyaiWebSoul <145985137+riyaiWebSoul@users.noreply.github.com>
2023-10-11 10:36:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
-const server = express();
 const fs = require('fs');
 
 const HomeRouter = require('./routes/home');
@@ -21,12 +20,14 @@ const PatientReview = require('./routes/PatientReview');
 const DrList = require('./routes/drList');
 const LoginIdRouter = require('./routes/loginId');
 const ImageUploadRouter = require('./routes/imagesUpload');
+
+const server = express();
 const PORT = process.env.PORT || 8080;
 
 // Connect to the MongoDB database
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/', {
+    await mongoose.connect('mongodb://localhost/your-database-name', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -38,11 +39,17 @@ async function connectToDatabase() {
 
 // Middleware to parse JSON request bodies
 server.use(express.json());
-server.use(morgan('default'));
+server.use(morgan('dev'));
 
 // Serve static files from the 'public' directory
 server.use(express.static('public'));
-server.use(cors());
+
+// CORS configuration
+server.use(cors({
+  origin: ['https://deploy-mean-1whq.vercel.app'],
+  methods: ['POST', 'GET'],
+  credentials: true,
+}));
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -50,7 +57,7 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Configure Multer for handling file uploads
+// Multer storage configuration for image uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, 'public', 'images'));
@@ -67,25 +74,44 @@ const upload = multer({ storage });
 // Define your routes using async functions
 async function setupRoutes() {
   server.use('/imageUploads', express.static('public/images'));
-  server.use('/products', productRouter.router);
-  server.use('/user', userRouter.router);
-  server.use('/about', AboutRouter.router);
-  server.use('/home', HomeRouter.router);
-  server.use('/appointments', AppointmentRouter.router);
-  server.use('/medical', MedicalRouter.router);
-  server.use('/MapingEcommerce', MapingEcommerceRouter.router);
-  server.use('/footer', FooterRouter.router);
-  server.use('/enquiry', EnquiryRouter.router);
-  server.use('/healingTouch', HealingTouch.router);
-  server.use('/PatientReview', PatientReview.router);
-  server.use('/drList', DrList.router);
-  server.use('/imageUpload', ImageUploadRouter.router);
-  server.use('/loginId', LoginIdRouter.router);
+  server.use('/products', productRouter);
+  server.use('/user', userRouter);
+  server.use('/about', AboutRouter);
+  server.use('/home', HomeRouter);
+  server.use('/appointments', AppointmentRouter);
+  server.use('/medical', MedicalRouter);
+  server.use('/MapingEcommerce', MapingEcommerceRouter);
+  server.use('/footer', FooterRouter);
+  server.use('/enquiry', EnquiryRouter);
+  server.use('/healingTouch', HealingTouch);
+  server.use('/PatientReview', PatientReview);
+  server.use('/drList', DrList);
+  server.use('/imageUpload', ImageUploadRouter);
+  server.use('/loginId', LoginIdRouter);
   server.use('/images', express.static('public/images'));
+
+  // Add a route to fetch data from MongoDB
+  server.get('/data', async (req, res) => {
+    try {
+      const db = await connectToDatabase(); // Connect to the database
+      const collection = db.collection('eikon'); // Replace with your collection name
+
+      // Query MongoDB to retrieve data (modify this query as needed)
+      const data = await collection.find({}).toArray();
+
+      // Send the retrieved data as a JSON response
+      res.json({ data });
+    } catch (err) {
+      console.error('Error fetching data from MongoDB:', err);
+      res.status(500).json({ error: 'Error fetching data from MongoDB' });
+    }
+  });
 }
 
+// Image URLs array
 const imageUrls = [];
 
+// Route to list image files
 server.get('/listImages', (req, res) => {
   const imageDir = path.join(__dirname, 'public', 'images');
 
@@ -102,33 +128,29 @@ server.get('/listImages', (req, res) => {
     });
 
     // Create an array of image URLs
-    const imageUrls = imageFiles.map((file) => `/${file}`);
+    const imageUrls = imageFiles.map((file) => `/images/${file}`);
 
     res.json({ images: imageUrls });
   });
 });
 
-
-  server.post('/imageUpload', upload.single('image'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).send('No file uploaded.');
-    }
+// Route to upload an image
+server.post('/imageUpload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
 
   // Get the image name from the uploaded file's filename
   const imageName = req.file.filename;
-console.log(imageName)
+
   // Add the image name to the imageUrls array
-  imageUrls.push(imageName);
+  imageUrls.push(`/images/${imageName}`);
 
   // You can send back the updated imageUrls array as a response
-  res.json({ imageUrl: `/${imageName}` });
+  res.json({ imageUrl: `/images/${imageName}` });
+});
 
-  // You can do further processing with the uploaded file here
-  // For now, just send a success response
-  res.send('File uploaded successfully.');
-}
-);
-
+// Route to delete an image
 server.delete('/deleteImage/:filename', (req, res) => {
   const filename = req.params.filename;
   const imagePath = path.join(__dirname, 'public', 'images', filename);
@@ -147,10 +169,9 @@ server.delete('/deleteImage/:filename', (req, res) => {
 
     res.json({ message: 'Image deleted successfully' });
   });
-});
+}
 
-
-// Start the server on port 8080
+// Start the server
 async function startServer() {
   await connectToDatabase();
   await setupRoutes();
